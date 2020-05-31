4b34ad5d4e309b701f7712930a82e59fe8c47d1d
Lucas Fujiwara <lucastfujiwara@gmail.com>
2020-05-31 17:54:34 -0300

    Inclusos dados de áreas de pesquisa e universidades

---
+++ database/seeders/assets/areas_de_pesquisa.js
@@ -0,0 +1,28 @@
+// https://portal.fiocruz.br/areas-de-pesquisa
+module.exports = [
+  'Entomologia, Biologia de Vetores e Reservatórios de agentes infecciosos',
+  'Microbiologia em Saúde e Ambiente',
+  'Virologia e Saúde',
+  'Parasitologia',
+  'Imunidade e Inflamação',
+  'Modelos experimentais de doenças',
+  'Doenças Crônicas e não-transmissíveis, medicina regenerativa',
+  'Nanotecnologia e novos materiais',
+  'Genômica, Proteômica, Biologia de Sistemas, Biologia Sintética, Computação Científica',
+  'Genética e Epidemiologia Molecular em Saúde, farmacogenética',
+  'Pesquisa Clínica e Ensaios Clínicos',
+  'Saúde e Gênero, Saúde do Idoso',
+  'Saúde Perinatal, da Criança e do Adolescente',
+  'Ambiente, Ecologia e Saúde',
+  'Epidemiologia, métodos estatísticos e quantitativos',
+  'Vigilância em Saúde',
+  'Políticas Públicas, Planejamento e Gestão em Saúde',
+  'Gestão de Ciência e Tecnologia em Saúde',
+  'Promoção da Saúde',
+  'Avaliação e Economia da Saúde',
+  'Informação e Comunicação em Ciências e Saúde',
+  'Sociologia, Antropologia, Filosofia e Saúde, Cultura e Sociedade',
+  'História, Saúde e Ciência',
+  'Educação em Ciências e Saúde',
+  'Educação não formal e divulgação das ciências e saúde'
+]
