a6b21fb1aacadb03b021f6fabde98b76882c5dbc
Lucas Fujiwara <lucastfujiwara@gmail.com>
2020-06-03 18:54:09 -0300

    Inserção de perfil a usuário

--- database/assets/full_restore.sql
+++ database/assets/full_restore.sql
@@ -99,3 +99,14 @@ CREATE FUNCTION inserir_perfil (codigo varchar(255), tipo varchar(255))
     id_perfil;
 
 $$;
+
+SET search_path TO exam_tracker;
+
+CREATE FUNCTION adicionar_perfil_a_usuario (id_perfil int, id_usuario int)
+  RETURNS void
+  LANGUAGE SQL
+  AS $$
+  INSERT INTO possui (id_perfil, id_usuario)
+    VALUES ($1, $2);
+
+$$;
---
+++ database/procedures/add_profile_to_user.sql
@@ -0,0 +1,10 @@
+SET search_path TO exam_tracker;
+
+CREATE FUNCTION adicionar_perfil_a_usuario (id_perfil int, id_usuario int)
+  RETURNS NULL
+  LANGUAGE SQL
+  AS $$
+  INSERT INTO possui (id_perfil, id_usuario)
+    VALUES ($1, $2);
+
+$$;
