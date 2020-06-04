54cd6527d9cea8da65ea6e3a6d32cabe40825dc2
Lucas Fujiwara <lucastfujiwara@gmail.com>
2020-06-04 18:10:51 -0300

    Adicionada procedure add_tutor_to_user

---
+++ database/procedures/add_tutor_to_user.sql
@@ -0,0 +1,10 @@
+SET search_path TO exam_tracker;
+
+CREATE FUNCTION adicionar_tutor_a_usuario (id_usuario_tutelado int, id_tutor int, id_servico int, id_perfil int, data_de_inicio date, data_de_termino date DEFAULT NULL)
+  RETURNS void
+  LANGUAGE SQL
+  AS $$
+  INSERT INTO tutelamento (id_usuario_tutelado, id_tutor, id_servico, id_perfil, data_de_inicio, data_de_termino)
+    VALUES ($1, $2, $3, $4, $5, $6);
+
+$$;
